bfd28c1fe6e9a061f3d5d3637b1df5bf27b1167c
Leo Vujanic <leonard.vujanic@gmail.com>
2018-10-13 20:39:01 +0200

    Indecision App - Update, add options filler form and reset button

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,28 +1,57 @@
 const app = {
 	title: 'Indecision App',
 	subTitle: 'What should I do?',
-	options: [
-		'One',
-		'Two',
-	]
+	options: [],
 };
 
 // root element
 const appRoot = document.getElementById('app');
 
 
-const template = (
-	<div>
-		<h1>{app.title}</h1>
-		{app.subTitle && <p>{app.subTitle}</p>}
-		<p>{app.options.length ? 'Here are your options:' : 'No options'}</p>
-		<ol>
-			<li>Item One</li>
-			<li>Item Two</li>
-		</ol>
-	</div>
-);
+const render = () => {
+	const template = (
+		<div>
+			<h1>{app.title}</h1>
+			{app.subTitle && <p>{app.subTitle}</p>}
+			<p>{app.options.length ? 'Here are your options:' : 'No options'}</p>
+			<button onClick={clearList}>Remove all</button>
+			<ol>
+				<li>Item One</li>
+				<li>Item Two</li>
+			</ol>
+			<form onSubmit={onFormSubmit}>
+				<input type="text" name="option"/>
+				<button>Add Option</button>
+			</form>
+		</div>
+	);
 
-ReactDOM.render(template, appRoot);
+	ReactDOM.render(template, appRoot);
+};
+
+const clearList = (e) => {
+	app.options = [];
+	render();
+};
+
+const onFormSubmit = (e) => {
+	e.preventDefault();
+
+	const optionEl = e.target.elements.option;
+	const optionValue = optionEl.value;
+
+	if (optionValue) {
+		app.options.push(optionValue);
+		optionEl.value = '';
+		render();
+	}
+};
+
+
+// initialize app
+render();
+
+
+
 
 
