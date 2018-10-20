887f40a8b5c1cc918451062c539d4b1531f59b34
Leo Vujanic <leonard.vujanic@gmail.com>
2018-10-20 18:43:08 +0200

    Expensify - es6 array destruction - basic example

---
+++ expensify-app/src/playground/array-destructuring.js
@@ -0,0 +1,19 @@
+const address = [
+	'Ulica Junaka 10',
+	'Zagreb',
+	'Croatia',
+	'10000'
+];
+
+
+console.log(`You are in ${address[1]}, ${address[2]}`);
+
+
+// basic example
+const [street, city, state, zip] = address;
+console.log(`You are in ${city}, ${state}`);
+
+
+// exclude some elements
+const [, city1, state1] = address;
+console.log(`You are in ${city1}, ${state1}`);
