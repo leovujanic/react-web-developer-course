c75ade379561542bcb43f3e180be6df92e4036ea
Leo Vujanic <leonard.vujanic@gmail.com>
2018-10-20 16:08:44 +0200

    Add boilerplate app

---
+++ boilerplates/v1/src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+
+const appRoot = document.getElementById('app');
+
+Modal.setAppElement(appRoot);
+
+ReactDOM.render(
+	<p>This is my boilerplate</p>,
+	appRoot
+);
