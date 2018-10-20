0bc19434dcb9f6f017c194445caf3121e805315b
Leo Vujanic <leonard.vujanic@gmail.com>
2018-10-21 00:45:39 +0200

    Expensify - link item and remove "remove" button

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import connect from "react-redux/es/connect/connect";
-
-import {removeExpense} from '../actions/expenses'
+import {Link} from 'react-router-dom';
 
 
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
 	<div>
-		<h2>{description}</h2>
+		<Link to={`/edit/${id}`}>
+			<h2>{description}</h2>
+		</Link>
 		<p>{amount} - {createdAt}</p>
-		<button onClick={() => {
-			dispatch(removeExpense({id}));
-		}}>X
-		</button>
 	</div>
 );
 
