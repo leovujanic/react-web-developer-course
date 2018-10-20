8ee9e2f4aa1bfc72edddd8e0e3d2614e1d058e8c
Leo Vujanic <leonard.vujanic@gmail.com>
2018-10-20 16:23:27 +0200

    Expensify - add first routes

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+
+const ExpenseDashboardPage = () => (
+	<div>
+		<p>This is from My Dashboard component</p>
+	</div>
+);
+
+
+const AddExpensePage = () => (
+	<div>
+		<p>This is from Add Expense page</p>
+	</div>
+);
+
+const routes = (
+	<BrowserRouter>
+		<div>
+			<Route path="/" component={ExpenseDashboardPage} exact={true}/>
+			<Route path="/create" component={AddExpensePage}/>
+		</div>
+	</BrowserRouter>
+);
 
 
 const appRoot = document.getElementById('app');
 
-Modal.setAppElement(appRoot);
-
 ReactDOM.render(
-	<p>This is my boilerplate</p>,
+	routes,
 	appRoot
 );
