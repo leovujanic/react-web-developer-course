fcf5c308f1866af23a0eed931059aaa82694d2a7
Leo Vujanic <leonard.vujanic@gmail.com>
2018-10-20 17:25:08 +0200

    Expensify - playing with redux - createState + basic actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,2 +1,44 @@
+import {createStore} from 'redux';
 
-console.log(101);
+
+const store = createStore((state = {count: 0}, action) => {
+	console.log('running');
+	switch (action.type) {
+		case 'INCREMENT':
+			return {
+				count: state.count + 1,
+			};
+		case 'DECREMET':
+			return {
+				count: state.count - 1,
+			};
+		case 'RESET':
+			return {
+				count: 0,
+			};
+		default:
+			return state
+	}
+});
+
+
+console.log(store.getState());
+
+
+store.dispatch({
+	type: 'INCREMENT'
+});
+
+store.dispatch({
+	type: 'INCREMENT'
+});
+
+store.dispatch({
+	type: 'DECREMET'
+});
+
+store.dispatch({
+	type: 'RESET'
+});
+
+console.log(store.getState());
