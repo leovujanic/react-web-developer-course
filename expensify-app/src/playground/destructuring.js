c7e804fda0f1b74820e17aee8150cd8a0c164444
Leo Vujanic <leonard.vujanic@gmail.com>
2018-10-20 18:39:21 +0200

    Expensify - es6 destruction - naming and default values

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -15,3 +15,25 @@ const {name, age} = person;
 
 
 console.log(`${name} is ${age}.`);
+
+if (person.location.temp && person.location.city) {
+	console.log(`It's ${person.location.temp} in ${person.location.city}`);
+}
+
+
+const {city, temp} = person.location;
+
+if (temp && city) {
+	console.log(`It's ${temp} in ${city}`);
+}
+
+// naming
+const {city: personCity, temp: temperature} = person.location;
+
+if (temperature && personCity) {
+	console.log(`It's ${temperature} in ${personCity}`);
+}
+
+// default value
+const {nameFoo: userName = 'Default'} = person;
+console.log(`It's ${userName}`);
