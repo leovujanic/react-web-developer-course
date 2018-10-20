ec9d7b0e4bf9baa38606d0f5e268535942c395e6
Leo Vujanic <leonard.vujanic@gmail.com>
2018-10-20 07:14:35 +0200

    Webpack - Test scss works

--- webpack-indecision-app/src/app.js
+++ webpack-indecision-app/src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss'
 
 
 const appRoot = document.getElementById('app');
